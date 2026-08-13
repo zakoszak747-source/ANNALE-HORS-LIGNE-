4f51c36b0db5403f9ff8bce2891484bc976d8fd9
zakoszak747-source <zakoszak747@gmail.com>
2026-08-13 06:15:56 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'annales-ayh-v1';
+const CACHE = 'annales-ayh-v2';
 const SHELL = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e=>{
